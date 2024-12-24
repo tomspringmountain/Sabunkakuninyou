b5d0a770602fd393697b912b15b11618ec263383
tomspringmountain <fow0p.mgtmp@gmail.com>
2024-12-24 19:17:28 +0900

    kadai4-1

---
+++ src/test/java/raisetech/Sabunkakuninyou/SabunkakuninyouApplicationTests.java
@@ -0,0 +1,13 @@
+package raisetech.Sabunkakuninyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabunkakuninyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
